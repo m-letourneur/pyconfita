4f47d0ffdd88ec51788a23b87a78b44d53e30cb2
m-letourneur <marc.letourneur.dev@gmail.com>
2023-07-24 09:20:02 +0200

    fix import

--- src/pyconfita/__init__.py
+++ src/pyconfita/__init__.py
@@ -2,5 +2,6 @@ from pyconfita.backend.environment.environment import Backend as EnvBackend
 from pyconfita.backend.vault.vault import Backend as VaultBackend
 from pyconfita.backend.file.file import Backend as FileBackend
 from pyconfita.backend.dict.dict import Backend as DictBackend
+from pyconfita.backend.string.string import Backend as StringBackend
 from pyconfita.logging_interface import LoggingInterface, DummyLoggingInterface
 from pyconfita.pyconfita import Confita
